84c42231dcee33a0748e7c6db90f4fa35d42e748
TerryFrench <tperraut@gmail.com>
2019-12-08 20:56:10 -0800

    Fixing board info and adding bltouch
    Fixing board info and adding bltouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -869,7 +869,7 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-//#define BLTOUCH
+#define BLTOUCH
 
 /**
  * Touch-MI Probe by hotends.fr
@@ -1168,7 +1168,7 @@
  *   With an LCD controller the process is guided step-by-step.
  */
 //#define AUTO_BED_LEVELING_3POINT
-//#define AUTO_BED_LEVELING_LINEAR
+#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
